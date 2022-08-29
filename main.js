6ec10f477570ec26db1599d6e05e5e44c8f7ee20
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-08-29 19:08:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
 logger: P({ level: 'silent'}),
-browser: ['TheMystic-Bot','Safari','1.0.0']
+browser: ['Shizu-Bot','Safari','1.0.0']
 }
 
 global.conn = makeWASocket(connectionOptions)
