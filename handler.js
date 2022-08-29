71e3d6f5d5742d9dec746c32723c6536b4611c2c
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-08-29 19:26:39 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -484,7 +484,7 @@ export async function participantsUpdate({ id, participants, action }) {
             if (chat.welcome) {
                 let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
                 for (let user of participants) {
-                    let pp = './src/avatar_contact.png'
+                    let pp = './src/avatar_contact.jpg'
                     try {
                         pp = await this.profilePictureUrl(user, 'image')
                     } catch (e) {
