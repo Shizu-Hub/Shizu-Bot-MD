417f94523d889497800a8d791dcc2243182ed344
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-08-31 12:20:51 +0100

    Create cuentas-hbo.js

---
+++ plugins/cuentas-hbo.js
@@ -0,0 +1,23 @@
+let handler  = async (m, { conn }) => {
+conn.reply(m.chat,`*┌────「 HBO 」─*\n*“${pickRandom(global.hbo)}”*\n*└────「 CUENTA GENERADA 」─*`, m)
+}
+handler.help = ['hbo']
+handler.tags = ['fun']
+handler.command = /^hbo/i
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.hbo = [
+  "Correo: ahbojm9@gmail.com Contraseña: cuenta123",
+  "Correo: ahbojm11@gmail.com Contraseña: cuenta123",
+  "Correo: wordofbin+2@gmail.com Contraseña: a1962080",
+  "Correo: wordofbin+3@gmail.com Contraseña: a1652100",
+  "Correo: wordofbin+4@gmail.com Contraseña: a1632086",
+  "Correo: wordofbin+5@gmail.com Contraseña: a1920203",
+  "Correo: wordofbin+9@gmail.com Contraseña: a1692101",
+  "Correo: wordofbin+10@gmail.com Contraseña: a4621057",
+
+] 
