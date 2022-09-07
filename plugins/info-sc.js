e82fd3605554baf9507fae7e02c78a6ec1f1be0a
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-09-07 10:46:12 +0100

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
-let texto1 = `*https://github.com/Shizu-Hub/Shizu-Bot*`
+let texto1 = `*https://github.com/Shizu-Hub/Shizu-Bot-MD*`
 let buttonMessage= {
-'document': { url: `https://github.com/Shizu-Hub/Shizu-Bot` },
+'document': { url: `https://github.com/Shizu-Hub/Shizu-Bot-MD` },
 'mimetype': `application/${document}`,
 'fileName': `「  𝑯𝒆𝒍𝒍𝒐 𝑾𝒐𝒓𝒍𝒅 」`,
 'fileLength': 99999999999999,
@@ -12,7 +12,7 @@ let buttonMessage= {
 'forwardingScore': 200,
 'isForwarded': true,
 'externalAdReply': {
-'mediaUrl': 'https://github.com/Shizu-Hub/Shizu-Bot',
+'mediaUrl': 'https://github.com/Shizu-Hub/Shizu-Bot-MD',
 'mediaType': 2,
 'previewType': 'pdf',
 'title': 'ᴇʟ ᴍᴇᴊᴏʀ ʙᴏᴛ ᴅᴇ ᴡʜᴀᴛsᴀᴘᴘ⁩',
