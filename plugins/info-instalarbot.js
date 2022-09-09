5038f6583f8e581241e85ba1d069e09ad5f5e945
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-09-09 15:11:58 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,18 +1,9 @@
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let text = `
-*—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙱𝙾𝚇𝙼𝙸𝙽𝙴 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/eC9TfKICpcY
-> Pagina Oficial: https://boxmineworld.com
-> Dashboard: https://dash.boxmineworld.com/home
-> Panel: https://panel.boxmineworld.com
-> Soporte: https://discord.gg/84qsr4v 
-
-------------------------------------
-
 *—◉ 𝚃𝚄𝚃𝙾𝚁𝙸𝙰𝙻 𝙰𝙲𝙸𝙳𝙸𝙲𝙽𝙾𝙳𝙴𝚂 𝙷𝙾𝚂𝚃*
-> Tutorial: https://youtu.be/nbjvreJ0tUk
-> Pagina: https://billing.acidicnodes.ml/register?ref=nSzLjTPd
-> Soporte: https://whatsapp.acidicnodes.com
+> Tutorial: https://youtu.be/rWzCNV2J8S0
+> Pagina Oficial: https://billing.acidicnodes.ml/register?ref=nSzLjTPd
+> Soporte: http://whatsapp.acidicnodes.com/
 
 ------------------------------------
 
