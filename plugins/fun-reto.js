6addf95f7b049cc12bcbb40d207ffefb19d5647e
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-09-09 16:53:52 +0100

    Update fun-reto.js

--- plugins/fun-reto.js
+++ plugins/fun-reto.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 𝙼𝚈𝚂𝚃𝙸𝙲 」─*`, m)
+conn.reply(m.chat,`*┌────「 𝚁𝙴𝚃𝙾 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 SHIZU 」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['fun']
