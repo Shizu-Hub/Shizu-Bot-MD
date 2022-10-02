c9ba63c239dc6ece77ac7f6a859b0e69af858572
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-10-02 12:29:11 +0100

    Update _wel&bye.js

--- plugins/_wel&bye.js
+++ plugins/_wel&bye.js
@@ -3,11 +3,11 @@ let stikerwelgc = "./src/welgc.webp"
 let stikerbyegc = "./src/byegc.webp"
 if (command == 'welcomegc') {
 conn.sendFile(m.chat, stikerwelgc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: '©𝐵𝑟𝑢𝑛𝑜𝑆𝑜𝑏𝑟𝑖𝑛𝑜', sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, thumbnail: imagen1}}})  
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: 'Shizu-Hub', sourceUrl: `https://github.com/Shizu-Hub/Shizu-Bot-MD`, thumbnail: imagen1}}})  
 }
 if (command == 'byegc') {
 conn.sendFile(m.chat, stikerbyegc, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: '©𝐵𝑟𝑢𝑛𝑜𝑆𝑜𝑏𝑟𝑖𝑛𝑜', sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, thumbnail: imagen1}}})
+contextInfo: { externalAdReply: { title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: 'Shizu-Hub', sourceUrl: `https://github.com/Shizu-Hub/Shizu-Bot-MD`, thumbnail: imagen1}}})
 }}
 handler.command = ['welcomegc', 'byegc']
 export default handler
