b6b6e1ead731bd4415a64c74464c84f635a86efc
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-10-21 18:25:12 +0100

    Update cuentas-primevideo.js

--- plugins/cuentas-primevideo.js
+++ plugins/cuentas-primevideo.js
@@ -12,6 +12,6 @@ function pickRandom(list) {
 }
 
 global.star = [
-  "Correo: pruevau36@gmail.com Contraseña: registrate en esta web https://dash.boxmineworld.com/register?ref=xGjnynVf y reclama tu cuenta en wa.e/34623442554 oferta valida hasta el 22/10/22",
+  "Registrate en esta web https://dash.boxmineworld.com/register?ref=xGjnynVf y reclama tu cuenta en wa.e/34623442554 oferta valida hasta el 22/10/22",
 
 ] 
