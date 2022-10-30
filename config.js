3daa3110717784cd265ced82ccc2ef0191e8c27b
Shizu-Hub <112402377+Shizu-Hub@users.noreply.github.com>
2022-10-30 10:30:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.prems = ['34623442554']
 
 global.packname = '(≧▽≦)'
 global.author = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗛𝘂𝗯⍟'
-global.wm = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗕𝗼𝘁⍟ V5.1'
+global.wm = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗕𝗼𝘁⍟ V5.2'
 global.igfg = '⍟𝗦𝗵𝗶𝘇𝘂 » 𝗕𝗼𝘁⍟'
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
 
