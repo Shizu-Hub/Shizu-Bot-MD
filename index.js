cf01a5e9ab5d1eab7ed5ed58433c2f496ed7debb
U M A R D E V I L <103053269+umar404@users.noreply.github.com>
2022-12-14 19:55:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Shizu - Bot\nWhatsApp Bot MD', {
+say('Umar - 404\nUd Md V1', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Shizu-Hub`, {
+say(`Bot creado por Umar Devil`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
