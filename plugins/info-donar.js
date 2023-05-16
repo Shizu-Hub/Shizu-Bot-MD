7b25d27cbf1bcc3c789ce936625afe24bb76397d
ayushdeveloper003 <119794831+ayushdeveloper003@users.noreply.github.com>
2023-05-16 16:19:26 +0530

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -4,30 +4,23 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
 *┏ ┅ ━━━━━━━━━ ┅ ━*
-*┇          「 𝐃𝐎𝐍𝐀𝐑 」*
+*┇          「 Donate 」*
 *┣ ┅ ━━━━━━━━━ ┅ ━*
 *┃ 𝙷𝙾𝙻𝙰 ${name} 💙*
 *┃*
-*┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
-*┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
+*┃ 👉🏻 Thanks for Using My Bot*
+*┃ Its really Appreciatable :𝟹*
 *┃*
-*┃ ➤ 𝙲𝙻𝙰𝚅𝙴:* 
-*┃ 646180192175788481*
-*┃ ➤ 𝙱𝙰𝙽𝙲𝙾: STP* 
-*┃ ➤ 𝙱𝙴𝙽𝙴𝙵𝙸𝙲𝙸𝙰𝚁𝙸𝙾:* 
-*┃ BRUNO SOBRINO* 
-*┃ ➤ 𝙲𝙾𝙽𝙲𝙴𝙿𝚃𝙾: APOYO*  
-*┃ ➤ 𝙿𝙰𝚈𝙿𝙰𝙻: https://www.paypal.me/NeKosmicOFC*
-*┃*
-*┃ 👉🏻 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙰𝙼𝙴 𝚂𝙸* 
-*┃ 𝙽𝙴𝙲𝙴𝚂𝙸𝚃𝙰𝚂 𝙼𝙰𝚂*
-*┃ 𝙳𝙰𝚃𝙾𝚂 𝚈 𝙿𝙰𝚁𝙰* 
-*┃ 𝙰𝙶𝚁𝙰𝙳𝙴𝙲𝙴𝚁𝚃𝙴 <𝟹*
-*┃ wa.me/34623442554*
+*┃ ➤ Number of Main developer:* 
+*┃ wa.me/16468338933*
+*┃ ➤ Country Of Ayush: India* 
+*┃ ➤ Donation:* 
+*┃ No Need* 
+*┃ ➤ Age: 16*  
 *┗ ┅ ━━━━━━━━━ ┅ ━*
 `.trim()
 conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/NeKosmicOFC', '𝙿𝙰𝚈𝙿𝙰𝙻', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
-handler.help = ['donasi']
+handler.help = ['donate']
 handler.tags = ['info']
 handler.command = /^dona(te|si)|donar|apoyar$/i
 export default handler
